2449f45c86cdfb560ed50a909fa0a8a500f6eb69
cuiweigeng <cuiweigeng@hotmail.com>
2017-03-02 03:12:57 +0800

    realCw use no futrue data

--- forex/MATLAB/saveData2.m
+++ forex/MATLAB/saveData2.m
@@ -1,16 +1,18 @@
-function saveData2(H, L, C, indPrd, lookForwardLen, dim, waveletLen, waveletParam, len, file1, file2)
+function saveData2(H, L, C, indPrd, ...
+                   lookForwardLen, dim, ...
+                   waveletLen, waveletParam, ...
+                   len, file1, file2)
 
 %% train Wavelet 
 trainData = zeros(len-lookForwardLen-waveletLen, dim, dim);
 verifyData = zeros(len-lookForwardLen-waveletLen, 1);
-fidTrain=fopen(file1, 'wb','ieee-be'); %´ó¶Ë
+fidTrain=fopen(file1, 'wb','ieee-be'); % big
 fidVerify=fopen(file2,'wb','ieee-be');
 fwrite(fidTrain, len-lookForwardLen-waveletLen, 'uint');
 fwrite(fidVerify, len-lookForwardLen-waveletLen, 'uint');
 
-
-realCw = wden(C, waveletParam.tptr, waveletParam.sorh, waveletParam.scal, ...
-    waveletParam.lev, waveletParam.wname) * 100000; 
+% realCw = wden(C, waveletParam.tptr, waveletParam.sorh, waveletParam.scal, ...
+%     waveletParam.lev, waveletParam.wname) * 100000; 
 
 % plot(C*100000,'k');hold on;plot(realCw,'r--');
 
@@ -36,12 +38,12 @@ for i=1:len-lookForwardLen-waveletLen
 %     maH = ma(tmpH, waveletLen, indPrd);
 %     maL = ma(tmpL, waveletLen, indPrd);
     
-    cla;
-    tmpC = wden(C(1:waveletLen+i-1), waveletParam.tptr, waveletParam.sorh, ...
-        waveletParam.scal, waveletParam.lev, waveletParam.wname) * 100000;
-    plot(C(range)*100000,'k');hold on;
-    plot(tmpC(range),'b*');hold on;
-    plot(realCw(range),'r--');
+%     cla;
+%     tmpC = wden(C(1:waveletLen+i-1), waveletParam.tptr, waveletParam.sorh, ...
+%         waveletParam.scal, waveletParam.lev, waveletParam.wname) * 100000;
+%     plot(C(range)*100000,'k');hold on;
+%     plot(tmpC(range),'b*');hold on;
+%     plot(realCw(range),'r--');
     
     maH = indicators(tmpH(end-indCalcLen:end), 'sma', indPrd);
     maL = indicators(tmpL(end-indCalcLen:end), 'sma', indPrd);
@@ -49,24 +51,14 @@ for i=1:len-lookForwardLen-waveletLen
 
     rsiC = rsindex(tmpC(end-indCalcLen:end), indPrd);
 
-    stochC = stoch(tmpC(end-indCalcLen:end), length(tmpC(end-indCalcLen:end)), indPrd);
+    stochC = stoch(tmpC(end-indCalcLen:end), ...
+        length(tmpC(end-indCalcLen:end)), indPrd);
 
     [mid, uppr, lowr] = bollinger(tmpC(end-indCalcLen:end), indPrd, 0, 2.0);
     
-    cciC = indicators([tmpH(end-indCalcLen:end), tmpL(end-indCalcLen:end), tmpC(end-indCalcLen:end)], 'cci' ,indPrd, indPrd, 0.015);
-    
-    
-    
-    
-    %% remove indPrd, time start at (waveletLen+indPrd)
-%     maH = maH(indPrd + 1:end);
-%     maL = maL(indPrd + 1:end);
-%     maC = maC(indPrd + 1:end);
-%     uppr = uppr(indPrd + 1:end);
-%     lowr = lowr(indPrd + 1:end);
-%     stochC = stochC(indPrd + 1:end);
-%     rsiC = rsiC(indPrd + 1:end);
-%     cciC = cciC(indPrd + 1:end);
+    cciC = indicators([tmpH(end-indCalcLen:end), ...
+        tmpL(end-indCalcLen:end), ...
+        tmpC(end-indCalcLen:end)], 'cci' ,indPrd, indPrd, 0.015);
     
     %% get the last waveletLen ---> train data
 %     trainData(i, 1, :) = normalization(maH(end-lookForwardLen+1:end));
@@ -79,7 +71,12 @@ for i=1:len-lookForwardLen-waveletLen
     trainData(i, 2, :) = normalization(cciC(end-dim+1:end));
     
     %% valid data
-    lookForwardCw = realCw(range(end)+lookForwardLen);
+%     rangeReal = [range, range(end)+1:range(end)+lookForwardLen];
+    realCw = wden(C(1:waveletLen+i+lookForwardLen-1), ...
+        waveletParam.tptr, waveletParam.sorh, waveletParam.scal, ...
+        waveletParam.lev, waveletParam.wname) * 100000; 
+
+    lookForwardCw = realCw(end);
     if lookForwardCw - realCw(range(end)) > 0
         verifyData(i) = 1;
     elseif lookForwardCw - realCw(range(end)) < 0
@@ -90,8 +87,8 @@ for i=1:len-lookForwardLen-waveletLen
     
     %%
 
-    for j=1:6
-        for k=1:6
+    for j=1:dim
+        for k=1:dim
             fwrite(fidTrain, trainData(i, j, k), 'uint');
         end
     end
@@ -100,7 +97,8 @@ for i=1:len-lookForwardLen-waveletLen
     %% info
     if(rem(i, 1000) == 0) 
         toc
-        fprintf('proc num = %d, total num = %d\n', i, len-lookForwardLen-waveletLen);
+        fprintf('proc num = %d, total num = %d\n', ...
+            i, len-lookForwardLen-waveletLen);
         tic
     end
 end
